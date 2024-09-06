e3a8e547986c26ed37ef951552653a0873ce5284
Kervin Francois <mrkary2010@gmail.com>
2024-09-06 08:29:26 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/studentlst/Student.java
@@ -0,0 +1,26 @@
+package com.mycompany.studentlst;
+
+/**
+ *
+ * @author MoaathAlrajab
+ * @modifier Kervin Francois
+ */
+public class Student {
+    
+    private String name;
+
+    // To Do 01: Add a constructor to this class
+    public Student(String name) {
+        this.name = name;
+    }
+
+    // To Do 02: Add setters and getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -0,0 +1,38 @@
+package com.mycompany.studentlst;
+
+import java.util.Scanner;
+
+/**
+ * @author MoaathAlrajab
+ * @modifier Kervin Francois
+ */
+public class StudentLst {
+
+    public static void main(String[] args) {
+        Student[] students= new Student[2];
+        Scanner inScanner = new Scanner(System.in);
+        
+        for(int i=0; i< students.length;i++){
+            System.out.print("Enter student's name: ");
+            String name = inScanner.nextLine();
+            Student student = new Student(name);
+            
+            students[i]= student;
+            
+        }
+        System.out.println(printOutput(students));
+    }
+    
+    public static String printOutput(Student[] students){
+        StringBuilder str = new StringBuilder();
+
+        // To Do 03: Complete the implementation of this method
+        for(Student student : students) {
+            str.append("The student's name is ").append(student.getName()).append(" \n");
+        }
+
+        return str.toString();
+    }
+    
+    
+}
